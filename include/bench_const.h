c0a41797c7304184667d71d318870d09f4b53852
li-ch <crischenli@gmail.com>
2017-03-04 18:26:59 +0800

    build script

--- include/bench_const.h
+++ include/bench_const.h
@@ -2,8 +2,8 @@
 #define BENCH_CONST_H_
 
 static const int SND_NUM = 1e5;
-static const int CLT_NUM = 1;
-static const int ITER_NUM = 64;
-static constexpr size_t MSG_LEN = (1 << 20);
+static const int CLT_NUM = 2;
+static const int ITER_NUM = 1e3;
+static constexpr size_t MSG_LEN = (1 << 21);
 
 #endif /* BENCH_H */
